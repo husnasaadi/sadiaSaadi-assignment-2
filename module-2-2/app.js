755d364b36fc0e5bb778f06c9ac890031f43a7c9
husnasaadi <160035721+husnasaadi@users.noreply.github.com>
2024-06-08 05:33:29 -0700

    Add files via upload

---
+++ module-2-2/app.js
@@ -0,0 +1,39 @@
+// // Q NO 1 
+
+// let exp = "faraz" || (true + false) + 5 && " " || "true";
+let exp = "faraz";
+
+// let exp1 = (1 + 2) + ++b || 5 && 0; // for b = 3;
+let exp1 = 7;
+
+// let exp2 = 32 && true - ++a && " " || "true"; // for a = 5;
+let exp2 = " ";
+
+// let exp3 = (true + 3) * 5 && undefined || "faraz" + 5;
+let exp3 = "faraz5";
+
+// let exp4 = 32 + "abc" || (true + false) + false || +5 || "true";
+let exp4 = "32abc";
+
+// let exp5 = false || (true + true) + 15 && "" || 50;
+let exp5 = 50;
+
+// let exp6 = (true + false) + 5 || false + 8 + "abc" || "true";
+let exp6 = 6;
+
+// let exp7 = "faraz" || false + 10 || "true";
+let exp7 = "faraz";
+
+// let exp8 = 12 + (false + false) + true && null || "faraz";
+let exp8 = "faraz";
+
+// let exp9 = (false + true + false + 2) || "faraz" && false || 1 + 10;
+let exp9 = 3;
+
+// let exp10 = (true + false) + 5 || false + 8 + "abc" || "true";
+let exp10 = 6;
+
+// let exp11 = (true + false) * 3 && "" || false + true - (12 + true + true + false);
+let exp11 = -13;
+
+
